b6a443c8e9115bc6cddaa1710ac12c329ee2b305
Gilexarr <tmenichel@gmail.com>
2017-10-28 15:17:50 -0400

    Position finalized

--- Assets/Scripts/RenderManager.cs
+++ Assets/Scripts/RenderManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class RenderManager : MonoBehaviour {
 
-    Sprite[] spriteSheet;
-    Sprite tileSprite;
+    public static Sprite[] spriteSheet;
 
 	// Use this for initialization
 	void Start () {
 
         PositionNodes();
+
+        spriteSheet = Resources.LoadAll<Sprite>("Sprites/Tiles");
         
     }
 	
@@ -29,8 +30,10 @@ public class RenderManager : MonoBehaviour {
         {
             for (int y = 0; y < Data.nodes.GetLength(1); y++)
             {
-                Data.nodes[x, y].positionInWorld.x = x*1f;
-                Data.nodes[x, y].positionInWorld.y = y*-0.5f;
+                float xPos = x;
+                float yPos = y;
+
+                Data.nodes[x, y].transform.position = new Vector2(xPos + 0.5f,-(yPos/2f));
             }
         }
     }
