65513ffec4edd3f2aa7e6cfc6c31831c21726ece
Gilexarr <tmenichel@gmail.com>
2017-10-28 15:20:15 -0400

    Revert "Position finalized"
    This reverts commit b6a443c8e9115bc6cddaa1710ac12c329ee2b305.

--- Assets/Scripts/RenderManager.cs
+++ Assets/Scripts/RenderManager.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class RenderManager : MonoBehaviour {
 
-    public static Sprite[] spriteSheet;
+    Sprite[] spriteSheet;
+    Sprite tileSprite;
 
 	// Use this for initialization
 	void Start () {
 
         PositionNodes();
-
-        spriteSheet = Resources.LoadAll<Sprite>("Sprites/Tiles");
         
     }
 	
@@ -30,10 +29,8 @@ public class RenderManager : MonoBehaviour {
         {
             for (int y = 0; y < Data.nodes.GetLength(1); y++)
             {
-                float xPos = x;
-                float yPos = y;
-
-                Data.nodes[x, y].transform.position = new Vector2(xPos + 0.5f,-(yPos/2f));
+                Data.nodes[x, y].positionInWorld.x = x*1f;
+                Data.nodes[x, y].positionInWorld.y = y*-0.5f;
             }
         }
     }
