e7c9c2ac190f59df1bbc01f403862a905cec623a
jxs2828 <jxs2828@IGM2025-12>
2017-10-27 22:58:31 -0400

    Some Data Work

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class Data : MonoBehaviour {
+public class Data : MonoBehaviour
+{
 
     public static Node[,] nodes;
 
-	void Start () {
-		
+	void Start ()
+    {
+        Stream inStream = new FileStream(Application.dataPath + "/Maps/map01.txt", FileMode.Open);
+        StreamReader reader = new StreamReader(inStream);
+
+        Debug.Log(reader.ReadLine());
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
