c580aa883c8af511cf7676563de3358975b11964
jxs2828 <jxs2828@IGM2435-03>
2017-10-28 14:56:57 -0400

    Fixed Data and Node

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    public Vector2 positionInArray;
-    public Vector2 positionInWorld;
+    public int xPositionInArray;
+    public int yPositionInArray;
     public Unit unitOccupyingSpace;
     public int material;
     public int wall;
-    public int movementRequirement;     //If -1, tile is impassible
-    public SpriteRenderer renderer;
-    public Sprite tileSprite;
+    public int movementRequirement;
+    public SpriteRenderer spriteRenderer;
 	
 	void Start ()
     {
-        renderer = gameObject.GetComponent<SpriteRenderer>();
+
+         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 	}
 	
 	
@@ -24,14 +24,12 @@ public class Node : MonoBehaviour
 
 	}
 
-    public Node Initialize(int matirial, int wall, int unit)
+    public Node Initialize(int x, int y, int matirial, int wall, int unit)
     {
+        xPositionInArray = x;
+        yPositionInArray = y;
+        spriteRenderer.sprite = RenderManager.spriteSheet[matirial + wall];
         return this;
-    }
-
-    private void OnMouseEnter()
-    {
-        
     }
 
 
