d5dcf84ead463d5f1ec3896a15fb338731c0c342
jxs2828 <jxs2828@IGM2435-03>
2017-10-28 13:40:05 -0400

    Changed Logic Manager

--- Assets/Scripts/LogicManager.cs
+++ Assets/Scripts/LogicManager.cs
@@ -4,27 +4,16 @@ using UnityEngine;
 
 public class LogicManager : MonoBehaviour
 {
-    //References
-    public Node[,] nodes;
-    
 
 
 	void Start ()
     {
-        nodes = new Node[2, 2];
+        
 	}
 	
 	
 	void Update ()
     {
-        // loop through the array of nodes and apply the correct texture
-        for (int x = 0; x < nodes.GetLength(0); x++)
-        {
-            for (int y = 0; y < nodes.GetLength(1); y++)
-            {
-                // trying to go into the assets folder and find sprites, then into the spritesheet
-                //nodes[x, y] = Resources.Load("Sprites");
-            }
-        }
+        
 	}
 }
