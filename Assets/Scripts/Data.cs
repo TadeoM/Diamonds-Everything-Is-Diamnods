1b90baec8d5b521ec69989f0f36c296dfd7e9df9
Gilexarr <tmenichel@gmail.com>
2017-10-28 19:06:17 -0400

    images

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -36,9 +36,6 @@ public class Data : MonoBehaviour
             // go through x index
             for(int x = 0; x < nodes.GetLength(0); x++)
             {
-                Debug.Log("X lengths: " + nodes.GetLength(0));
-                Debug.Log("Y lengths: " + nodes.GetLength(1));
-                Debug.Log("x: " + x + " y: " + y);
                 // for the first one (0,) spawn a node
                 nodes[x, y] = Instantiate(nodePrefabRef).GetComponent<Node>();
                 // initialize it, and give the data of the 3 numbers
