589d76fbfd0fafc5814f239c11fb2b93374eae7e
Gilexarr <tmenichel@gmail.com>
2017-10-27 22:12:53 -0400

    Starter classes

---
+++ Assets/Scripts/Units/FireFighters.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFighters : Unit {
+
+    public GameObject gun;
+
+	// Use this for initialization
+	void Start () {
+        movementSpeed = 3;
+        health = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckDead();
+        CheckInputs();
+	}
+
+    /// <summary>
+    /// check for inputs
+    /// </summary>
+    void CheckInputs()
+    {
+        // player presses S
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ShowRange();
+        }
+    }
+
+    void ShowRange()
+    {
+        // tint the area that is valid to shoot
+
+        // player chooses a place to shoot and presses left mouse button
+        if (Input.GetButtonDown("fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // shoot at target location
+    }
+}
---
+++ Assets/Scripts/Units/FireMinion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMinion : Unit {
+
+	// Use this for initialization
+	void Start () {
+        movementSpeed = 2;
+        health = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    Vector2 Move(Vector2 targetLocation)
+    {
+
+        return new Vector2(0,0);
+    }
+}
