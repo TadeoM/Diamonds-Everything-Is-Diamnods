6113e701c321e2e5298fd56cce281c3a98e79365
jxs2828 <jxs2828@IGM2025-12>
2017-10-27 21:46:43 -0400

    Groundwork for Node and Logic classes

---
+++ Assets/Scripts/LogicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicManager : MonoBehaviour
+{
+    //References
+
+
+    //Variables
+    public Node[,] nodes;
+
+
+	void Start ()
+    {
+        nodes = new Node[10, 10];
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+
+    //public Unit unitOccupyingSpace;
+    public int texture;
+    public int movementRequirement;
+	
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+
+	}
+}
