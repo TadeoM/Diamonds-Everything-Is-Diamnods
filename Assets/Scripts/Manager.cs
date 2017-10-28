30bfb9434d562ee4ccef952c25cecc94e3cba6a8
cfc5762 <cfc5762@IGM2435-02>
2017-10-28 19:05:09 -0400

    Made Firemen Spawn

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Manger : MonoBehaviour
+public class Manager : MonoBehaviour
 {
     bool friendlyturn = false;
     public static GameObject turnUnit;
-    public static GameObject[] prefabs;
+    public GameObject[] Prefabs;
     public static List<GameObject> allUnits = new List<GameObject>();
+    
     // Use this for initialization
-    void Start()
+    void Awake()
     {
-
+        
     }
-    public static GameObject Spawn(int[] arrayposition,int identifier)
+    public GameObject Spawn(int[] arrayposition,int identifier)
     {
         GameObject current = null;
         if (identifier > 0 && identifier <= 2)
         {
-            current = Instantiate(prefabs[identifier - 1], new Vector3(arrayposition[0]+arrayposition[1],-(arrayposition[1]/2)+arrayposition[0]/2-.5f), Quaternion.identity);
+            current = Instantiate(Prefabs[identifier - 1], new Vector2(arrayposition[0] + arrayposition[1], (-(arrayposition[1] / 2f) + arrayposition[0] / 2f) - .2f), Quaternion.identity);
             current.GetComponent<Unit>().arrayPosition = arrayposition;
             allUnits.Add(current);
         }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+    //References
+    public Manager managerRef;
+
+    bool m_down=false;
+    bool m_hover=false;
     public int xPositionInArray;
     public int yPositionInArray;
     public Unit unitOccupyingSpace;
@@ -14,7 +19,7 @@ public class Node : MonoBehaviour
 	
 	void Awake ()
     {
-         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 	}
 	
 	
@@ -28,11 +33,29 @@ public class Node : MonoBehaviour
         xPositionInArray = x;
         yPositionInArray = y;
         spriteRenderer.sprite = RenderManager.spriteSheet[matirial + wall];
-
+        if (unit > 0 && unit < 3)
+        {
+            managerRef.Spawn(new int[] { x, y }, 1);
+        }
         //unitOccupyingSpace = GameManager.Spawn(unit).G
 
         return this;
     }
-
+    private void OnMouseEnter()
+    {
+        m_hover = true;
+    }
+    private void OnMouseExit()
+    {
+        m_hover = false;
+    }
+    private void OnMouseDown()
+    {
+        m_down = true;
+    }
+    private void OnMouseUp()
+    {
+        m_down = false;
+    }
 
 }
