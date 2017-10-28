1f849e575ae38a9a1b55f22040db62004b27fe7c
cfc5762 <cfc5762@IGM2435-02>
2017-10-28 14:56:49 -0400

    movement

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 public class Unit : MonoBehaviour {
 
     // variables
+    public GameObject turnMinion;//if turnminion == this.gameobject it is your turn
     public int health;
     public int movementSpeed;
-
+    private bool prevMove;
+    private Vector3 desiredWorldpos;
+    public bool Moving { get { return (desiredWorldpos != transform.position); } }
+    private int framesTraveled=0;
 	// Use this for initialization
 	void Start () {
-		
+        desiredWorldpos = transform.position;
+        prevMove = Moving;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    public void Move(Vector3 finalpos)
+    {
+        desiredWorldpos = finalpos;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        #region Movement
+       
+        Vector3 currentMovement = transform.position - desiredWorldpos;
+        currentMovement = currentMovement / (20-framesTraveled);
+        transform.position += currentMovement;
+        if (prevMove && !Moving)
+        {
+            movementSpeed--;
+        }
+        if (Moving)
+        {
+            framesTraveled++;
+        }
+        else { framesTraveled = 0; }
+        prevMove = Moving;
+        #endregion
+
+    }
 
     public void CheckDead()
     {
