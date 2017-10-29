9ff135572087e6346100a78484e5dfe94a62ff08
jxs2828 <jxs2828@IGM2435-03>
2017-10-28 21:49:24 -0400

    Added Selection logic

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -6,14 +6,9 @@ public class Node : MonoBehaviour
 {
     //References
     public Manager managerRef;
-    public bool selected = false;
-    bool m_down=false;
-    bool m_hover=false;
     public int xPositionInArray;
     public int yPositionInArray;
     public Unit unitOccupyingSpace;
-    public int material;
-    public int wall;
     public int movementRequirement;
     public SpriteRenderer spriteRenderer;
 	
@@ -25,7 +20,11 @@ public class Node : MonoBehaviour
 	
 	void Update ()
     {
-
+        spriteRenderer.color =
+            new Color(
+                spriteRenderer.color.r + 10,
+                spriteRenderer.color.g + 10,
+                spriteRenderer.color.b + 10);
 	}
 
     public Node Initialize(int x, int y, int matirial, int wall, int unit)
@@ -47,16 +46,19 @@ public class Node : MonoBehaviour
 
         return this;
     }
-    private void OnMouseOver()
-    {
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            selected = true;
-        }
-    }
+
     private void OnMouseDown()
     {
-        Debug.Log(xPositionInArray + ", " + yPositionInArray);
+        if (Manager.activeNode != this)
+        {
+            Debug.Log(xPositionInArray + ", " + yPositionInArray + " selected");
+            Manager.activeNode = this;
+        }
+        else
+        {
+            Debug.Log(xPositionInArray + ", " + yPositionInArray + " deselected");
+            Manager.activeNode = null;
+        }
     }
 
 }
